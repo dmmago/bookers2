3f0d4c70db0723471b6c54d349d8d99c78126b74
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-02-18 10:05:03 +0000

    カラム追加前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -15,28 +15,30 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
 
-  
+
 
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  
-  
+
+
   def index
     @users = User.all
+    
+
+
   end
 
   def show
@@ -13,7 +16,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
@@ -22,9 +25,9 @@ class UsersController < ApplicationController
 end
 
 
-private 
+private
 def user_params
- params.require(:user).permit(:name, :profile_image)
+ params.require(:user).permit(:name, :introduction, :profile_image)
 end
 
 def is_matching_login_user
