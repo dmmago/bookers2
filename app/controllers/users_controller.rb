21cbdef56188f057cb039c6138fe9bfbe119379d
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-02-18 16:51:47 +0000

    残りヴァリテーション、共通化、デザイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    
+    @user = User.new
 
 
   end
@@ -11,16 +11,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = 'You have updated user successfully.'
+      redirect_to user_path(@user)
+    else
+      rander :edit
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about', as: 'about'
 
-  resources :users, only: [:new, :index, :show, :edit, :destroy]
+  resources :users, only: [:new, :index, :show, :edit, :destroy, :update]
   resources :books, only: [:new, :create, :index, :edit, :show, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
